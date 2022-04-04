492473693a5e8391bdc4ad4543f11aa855d7b88f
James Winchester <wildrover254@gmail.com>
2022-04-04 13:54:12 -0400

    Added code comments
    Comments added to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
+//Selects the grid container
 const container = document.querySelector('#container');
+
+//Uses a loop to create the initial grid
 for(let i = 1; i<= 256; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     container.appendChild(cell);   
 }
 
+//Function to change the color of cells
 function colorChange(e) {
     this.style.backgroundColor = 'black';
 }
 
+//Target all cells and apply event listener
 const cells = document.querySelectorAll('.cell');
 cells.forEach(cell => cell.addEventListener('mouseover', colorChange));
